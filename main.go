263642ef6414d102cb6a70f6aeafa40cbd44578f
kevin ndirangu <placbo2@gmail.com>
2026-06-25 11:15:11 +0300

    ...

--- main.go
+++ main.go
@@ -37,14 +37,29 @@ func (t1 *TodoList) MarkDone(id int) error {
 			return nil
 		}
 	}
-		return fmt.Errorf("todo %d not found", id)
+	return fmt.Errorf("todo %d not found", id)
+}
+
+func (t1 *TodoList) Remove(id int) error {
+	for i := range t1.items {
+		if t1.items[i].ID == id {
+			t1.items = append(t1.items[:i], t1.items[i+1:]...)
+				return nil
+		}
+	
+	}
+
+	return fmt.Errorf("todo %d not found", id)
 }
 
 func main() {
 	todos := NewTodoList()
 	todos.Add("learn go")
 	todos.Add("Build API")
-	todos.MarkDone(20)
-	err := todos.MarkDone(99)
-	fmt.Println(err)
+	todos.Add("deploy App")
+	fmt.Println("before:")
+	fmt.Println(todos.items)
+	todos.Remove(2)
+	fmt.Println("after:")
+	fmt.Println(todos.items)
 }
