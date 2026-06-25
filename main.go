b9323b84af929e8b099d8f780956cc03380ee5f5
kevin ndirangu <placbo2@gmail.com>
2026-06-25 08:36:43 +0300

    NewTodoList & constructor func

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
-import "fmt"
-
-type Box struct {
-	n int
+type Todo struct {
+	Title string
+	ID    int
+	Done  bool
 }
 
-func incByValue(b Box) {
-	b.n++
-}
-func incByPointer(b *Box) {
-	b.n++
+type TodoList struct {
+	items  []Todo
+	NextId int
 }
 
-func main() {
-	box := Box{n: 1}
-	incByValue(box)
+func NewTodoList() *TodoList {
+	return &TodoList{items: []Todo{}, NextId: 1}
+}
 
-	fmt.Println(box)
-
-	incByPointer(&box)
-	fmt.Println(box)
+func (t1 *TodoList) Add(title string) Todo {
+	t := Todo{ID: t1.NextId, Title: title}
+	t1.items = append(t1.items, t)
+	t1.NextId++
+	return t
 }
